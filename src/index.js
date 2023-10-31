ceceebe0171c1c0f05d4601cee5ff27893507157
Mark Wiemer <mwiemer@microsoft.com>
2023-10-31 14:41:10 -0700

    Tree shaking pre-side-effects

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import _ from 'lodash';
+import { cube } from "./math.js";
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement("pre");
+
+  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
+    "\n\n"
+  );
+
+  return element;
+}
+
+document.body.appendChild(component());
